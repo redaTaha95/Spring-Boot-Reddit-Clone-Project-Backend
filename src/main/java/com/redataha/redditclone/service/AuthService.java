5bc0d47c1b1eb31ce2d25305fcaf54dd97789f91
redaTaha95 <redataha27@gmail.com>
2022-08-24 13:56:19 +0100

    Activating user by token

--- src/main/java/com/redataha/redditclone/service/AuthService.java
+++ src/main/java/com/redataha/redditclone/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.redataha.redditclone.service;
 
 import com.redataha.redditclone.dto.RegisterRequest;
+import com.redataha.redditclone.exceptions.SpringRedditException;
 import com.redataha.redditclone.model.NotificationEmail;
 import com.redataha.redditclone.model.User;
 import com.redataha.redditclone.model.VerificationToken;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -51,5 +53,19 @@ public class AuthService {
 
         verificationTokenRepository.save(verificationToken);
         return token;
+    }
+
+    public void verifyAccount(String token) {
+        Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
+        verificationToken.orElseThrow(() -> new SpringRedditException("Invalid Token"));
+        fetchUserAndEnable(verificationToken.get());
+    }
+
+    @Transactional
+    private void fetchUserAndEnable(VerificationToken verificationToken) {
+        String username = verificationToken.getUser().getUsername();
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new SpringRedditException("User not found with name" + username));
+        user.setEnabled(true);
+        userRepository.save(user);
     }
 }
